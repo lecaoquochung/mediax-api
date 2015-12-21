0493333f2947a5b304234de0913778451d98bd46
Hung Le <lecaoquochung@gmail.com>
2015-12-21 17:14:34 +0900

    Plugin: Auth API & CRUD Stop

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -202,6 +202,5 @@ DispatcherFactory::add('ControllerFactory');
 Type::build('date')->useLocaleParser();
 Type::build('datetime')->useLocaleParser();
 
-Plugin::load('Crud');
-
-Plugin::load('ADmad/JwtAuth');
+// Plugin::load('Crud');
+// Plugin::load('ADmad/JwtAuth');
